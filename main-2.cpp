0f7f6658e28d1be5359e9df7c4b8328c61525d4a
cr1stiang <155077630+cr1stiang@users.noreply.github.com>
2023-12-28 13:36:39 +0000

    my first commit

--- main-2.cpp
+++ main-2.cpp
@@ -6,8 +6,8 @@ int main (void){
     int in1;
 
     std::cin >> in1;
-    std::cout << "I'm cristi 2 " << in12;
+    std::cout << "I'm cristi " << in1;
 }
 // go to build folder : cd ./build
-// build executable : cmake --build .d
+// build executable : cmake --build .
 // run executable : ./app
